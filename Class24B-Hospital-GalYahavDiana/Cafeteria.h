cbe5e4f805fe94f7e5b52770ce0357df084d1859
Gal Angel <122540669+GalAngel15@users.noreply.github.com>
2024-05-29 15:48:40 +0300

    update 2

--- Class24B-Hospital-GalYahavDiana/Cafeteria.h
+++ Class24B-Hospital-GalYahavDiana/Cafeteria.h
@@ -1,1 +1,20 @@
 #pragma once
+
+#ifndef __CAFETERIA_H
+#define __CAFETERIA_H
+#include "Building.h"
+#include "CafeteriaWorker.h"
+
+//#define ID_SIZE 9
+class Hospital : public Building
+{
+private:
+	CafeteriaWorker** cafeteriaWorkers;
+
+public:
+	Hospital(const char* firstName, const char* lastName, const int id);
+	bool hospitalize_patient(Patient* patient);
+	~Hospital();
+};
+
+#endif // __CAFETERIA_H
