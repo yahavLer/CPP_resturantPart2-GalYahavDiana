6be13cd7dcc8c2d5d279a773c106eb0f28272d29
yahavLer <118624372+yahavLer@users.noreply.github.com>
2024-05-30 12:41:17 +0300

    updete
    update

--- Class24B-Hospital-GalYahavDiana/Nurse.h
+++ Class24B-Hospital-GalYahavDiana/Nurse.h
@@ -1,1 +1,21 @@
 #pragma once
+
+#ifndef __NURSE_H
+#define __NURSE_H
+#include "Worker.h"
+#include "Patient.h"
+class Nurse : public Worker
+{
+private:
+
+	Patient** patientsGotCare;
+
+public:
+	Nurse();
+	void addPatient(const Patient* patient);
+	bool callDoctor(const Doctor* Doctor);
+	~Nurse();
+};
+
+#endif // __NURSE_H
+
