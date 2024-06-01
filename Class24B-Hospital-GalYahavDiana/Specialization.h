1eedaf03729a415652c5a8950d7d6fd4ab48bf72
yahavLer <118624372+yahavLer@users.noreply.github.com>
2024-06-01 10:28:19 +0300

    spesalization
    update

--- Class24B-Hospital-GalYahavDiana/Specialization.h
+++ Class24B-Hospital-GalYahavDiana/Specialization.h
@@ -1,1 +1,22 @@
 #pragma once
+
+#ifndef __SPECIALIZATION_H
+#define __SPECIALIZATION_H
+
+class Specialization 
+{
+private:
+	char* nameOfSpecialization;
+	Doctor** listOfDoctorWithSpecial;
+	int numOfDoctors;
+
+public:
+	Specialization(char* nameOfSpecialization);
+	Specialization(const Specialization& other);
+	bool setNameOfArtical(const char* nameOfArtical);
+	bool addResearcher(const Researcher& researcher);
+	void printSpecialization() const;
+	~Specialization();
+};
+
+#endif // __SPECIALIZATION_H
